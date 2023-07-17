6e36c9dfefd605dde8bfcb1ff663481f4021f78b
Harsh Patel <harsh3608@gmail.com>
2023-07-17 11:39:03 +0530

    added necessary components and metioned routes, lazy loading

--- src/app/students/students-routing.module.ts
+++ src/app/students/students-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingComponent } from './listing/listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentsRoutingModule } from './students-routing.module';
+import { ListingComponent } from './listing/listing.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { ViewComponent } from './view/view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListingComponent,
+    AddComponent,
+    UpdateComponent,
+    ViewComponent
+  ],
   imports: [
     CommonModule,
     StudentsRoutingModule
